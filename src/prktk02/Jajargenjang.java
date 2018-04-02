3894ac5a46eeabef59a964553d6d790e5ccabba4
Siti Al Irsyadiyyah <32434743+irsya123@users.noreply.github.com>
2018-04-02 22:26:54 +0800

    Membuat atribut dan Behavior

--- src/prktk02/Jajargenjang.java
+++ src/prktk02/Jajargenjang.java
@@ -1,4 +1,14 @@
 package prktk02;
 public class Jajargenjang {
     
+    double alas;
+    double tinggi;
+    
+    void cetakinfo(){
+        System.out.println("================");
+        System.out.println("Alas :"+alas);
+        System.out.println("Tinggi :"+tinggi);
+        System.out.println("=================");
+      
+    }
 }
