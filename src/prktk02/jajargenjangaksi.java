1e5803a55243393628d82be69c08023a47e776cc
Siti Al Irsyadiyyah <32434743+irsya123@users.noreply.github.com>
2018-04-02 22:36:41 +0800

    Mengisi nilai atribut

--- src/prktk02/jajargenjangaksi.java
+++ src/prktk02/jajargenjangaksi.java
@@ -4,8 +4,8 @@ public class jajargenjangaksi {
     public static void main(String[] args) {
       
         Jajargenjang jg1 =new Jajargenjang();
-        
-        
+        jg1.alas = 15;
+        jg1.tinggi = 25;
     }
     
 }
